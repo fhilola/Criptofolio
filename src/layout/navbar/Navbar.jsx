406d944f861edde79a68dfbd986f9854f3c2db18
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-10 21:47:18 +0500

    navbar added

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
+import './Navbar.scss'
 import logo from '../../assets/images/logo.svg'
-import { Container } from '../../utils'
+import { Button, Container } from '../../utils'
 
 const Navbar = () => {
   return (
@@ -8,7 +9,15 @@ const Navbar = () => {
         <div className="nav__wrapper">
           <div className="nav__logo">
             <img src={logo} alt="" />
-            <h1></h1>
+            <h1 className='navbar__title'>CRYPTOFOLIO</h1>
+          </div>
+          <div className="nav__action">
+            <select>
+              <option value="usd">USD</option>
+              <option value="rub">RUB</option>
+              <option value="uzb">UZB</option>
+            </select>
+            <Button text={'watch list'}/>
           </div>
         </div>
       </Container>
