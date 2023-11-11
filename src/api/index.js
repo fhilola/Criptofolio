036135099c89ac09e0e2a6aefbba07d673f4534e
fhilola <hilolafayoziddinova14@gmail.com>
2023-11-11 06:22:23 +0500

    apiInstance created

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+ const apiInstance = axios.create({
+    baseURL: proccess.env.REACT_APP_API_URL,
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    timeout: 
+ })
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,12 +1,21 @@
 import './Home.scss'
-import { Container } from '../../utils'
 import hero from '../../assets/images/hero.jpg'
+import { Container } from '../../utils'
 
 const Home = () => {
   return (
     <main>
         <div className="hero__wrapper">
           <img src={hero} alt="" />
+        </div>
+        <div className="hero__content">
+          <Container>
+            <h2>CRYPTOFOLIO WATCH LIST</h2>
+            <p>Get all the Info regarding your favorite Crypto Currency</p>
+            <div className="carusel__wrapper">
+              
+            </div>
+          </Container>
         </div>
     </main>
   )
